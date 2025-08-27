cc92389a07d028c3cadfe8b2c69d911aace13737
fisheeesh <swanphyo444@gmail.com>
2025-08-27 23:54:44 +0700

    chore: backend server setup

---
+++ backend/src/app.ts
@@ -0,0 +1,43 @@
+import compression from "compression"
+import cors from "cors"
+import express, { NextFunction, Request, Response } from "express"
+import helmet from "helmet"
+import morgan from "morgan"
+import cookieParser from "cookie-parser"
+import { limiter } from "./middlewares/rate-limitter"
+
+export const app = express()
+
+var whitelist = ['http://localhost:5173', 'http://localhost:4000']
+var corsOptions = {
+    origin: function (origin: any, callback: (err: Error | null, origin?: any) => void) {
+        //* Allow requests with no origin (like mobile apps or curl requests)
+        if (!origin) return callback(null, true)
+        if (whitelist.includes(origin)) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
+    //* Allow cookies or authorization header
+    credentials: true
+}
+
+app.use(morgan("dev"))
+    .use(express.urlencoded({ extended: true }))
+    .use(express.json())
+    .use(cors(corsOptions))
+    .use(helmet())
+    .use(compression({}))
+    .use(limiter)
+    .use(cookieParser())
+
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+    const status = error.status || 500
+    const message = error.message || 'Server Error.'
+    const errorCode = error.code || "Error_Code"
+
+    res.status(status).json({
+        message, error: errorCode
+    })
+})
