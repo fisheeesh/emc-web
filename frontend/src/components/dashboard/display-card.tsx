a1db4fdb69f0ff1abb042819ae982c2c5499051f
fisheeesh <swanphyo444@gmail.com>
2025-08-31 16:09:06 +0700

    chore: created attendance card

---
+++ frontend/src/components/dashboard/display-card.tsx
@@ -0,0 +1,23 @@
+import { cn } from "@/lib/utils";
+import { FaUsers } from "react-icons/fa";
+import { MdPlaylistAddCheckCircle } from "react-icons/md";
+
+interface Props {
+    data: number,
+    type: string
+}
+
+export default function DisplayCard({ data, type }: Props) {
+
+    return (
+        <div className="bg-white dark:border dark:border-slate-700 dark:bg-slate-900 dark:text-slate-50 border border-white w-1/2 lg:w-full drop-shadow-lg py-5 px-6 lg:py-12 rounded-md flex justify-between items-center">
+            <div>
+                <h1 className="text-2xl lg:text-4xl font-en">{data}</h1>
+                <p className={cn(type !== 'total' && 'text-green-500', 'text-sm lg:text-xl')}>{type === 'total' ? 'Total Employees' : 'Present'}</p>
+            </div>
+            <div className="bg-gray-100 dark:bg-slate-700 rounded-full p-2.5 lg:p-3">
+                {type === 'total' ? <FaUsers className="text-blue-500 size-5 lg:size-9" /> : <MdPlaylistAddCheckCircle className="text-green-500 size-5 lg:size-9" />}
+            </div>
+        </div>
+    )
+}
--- frontend/src/pages/dashboard/attendance.tsx
+++ frontend/src/pages/dashboard/attendance.tsx
@@ -1,11 +1,17 @@
+import DisplayCard from "@/components/dashboard/display-card"
 import useTitle from "@/hooks/useTitle"
 
 export default function AttendanceDashboardPage() {
     useTitle("Attendance Dashboard")
-    
+
     return (
-        <div>
-            attendence
-        </div>
+        <section className="flex flex-col justify-center items-center w-full gap-3">
+            <div className="flex flex-col lg:flex-row items-center justify-between w-full gap-3 lg:h-[343px]">
+                <div className="flex lg:flex-col gap-3 w-full lg:w-1/3">
+                    <DisplayCard data={25} type='total' />
+                    <DisplayCard data={23} type='present' />
+                </div>
+            </div>
+        </section>
     )
 }
