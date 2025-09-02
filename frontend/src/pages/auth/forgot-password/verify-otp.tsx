8991a738eb4c2a4ebf6fb7ba1a3b9ef22f245462
fisheeesh <swanphyo444@gmail.com>
2025-09-02 23:52:54 +0700

    chore: created verify otp for forgot password ui

---
+++ frontend/src/pages/auth/forgot-password/verify-otp.tsx
@@ -0,0 +1,13 @@
+import { VerifyOTPForm } from "@/components/auth/verify-otp-form"
+import useTitle from "@/hooks/use-title"
+
+export default function VerifyOTP() {
+    useTitle("Forget Password")
+    return (
+        <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+            <div className="w-full max-w-md">
+                <VerifyOTPForm />
+            </div>
+        </div>
+    )
+}
