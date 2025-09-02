4541c3749f00cb72df4e1879c97d7f5b81a0f944
fisheeesh <swanphyo444@gmail.com>
2025-09-02 23:56:41 +0700

    feat: created reusable create & edit modal for emp data manipulation

--- frontend/src/components/modals/create-edit-emp-modal.tsx
+++ frontend/src/components/modals/create-edit-emp-modal.tsx
@@ -7,36 +7,42 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm, type SubmitHandler } from "react-hook-form";
 import z from "zod";
 import { UserCheck } from "lucide-react";
+import { useRef } from "react";
+import { createEmpSchema } from "@/lib/validators";
 
 const EMP_POSITIONS = ["Intern", "Junior Developer", "Developer", "Senior Developer", "Team Lead"] as const;
 const EMP_ROLES = ["Employee", "Manager", "HR", "Admin", "Super Admin"] as const;
 const EMP_JOB_TYPES = ["Full-time", "Part-time", "Contract", "Internship"] as const;
 
-const formSchema = z.object({
-    email: z.string().email(),
-    password: z.string().min(8, "Password must be at least 8 characters long"),
-    name: z.string().min(2, "Name is too short"),
-    image: z
-        .any()
-        .refine(
-            (file) => !file || file instanceof File || (Array.isArray(file) && file[0] instanceof File),
-            "Invalid file"
-        )
-        .optional(),
-    department: z.string(),
-    position: z.enum(EMP_POSITIONS),
-    role: z.enum(EMP_ROLES),
-    jobType: z.enum(EMP_JOB_TYPES),
-});
-
 export default function CreateEditEmpModal() {
+    const fileInputRef = useRef<HTMLInputElement>(null)
+
+
     const form = useForm({
-        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+            firstName: "",
+            lastName: "",
+            image: null,
+            department: "",
+            position: "Intern",
+            role: "Employee",
+            jobType: "Contract",
+        },
+        resolver: zodResolver(createEmpSchema),
     });
 
-    const onHandleSubmit: SubmitHandler<z.infer<typeof formSchema>> = async (values) => {
+    const onHandleSubmit: SubmitHandler<z.infer<typeof createEmpSchema>> = async (values) => {
         console.log(values)
     }
+
+    const getFileName = () => {
+        const file = fileInputRef.current?.files?.[0]
+        return file ? file.name : "No file chosen"
+    }
+
+    const isWorking = form.formState.isSubmitting
 
     return (
         <DialogContent className="w-full mx-auto max-h-[90vh] overflow-y-auto sm:max-w-[800px] no-scrollbar">
@@ -55,70 +61,73 @@ export default function CreateEditEmpModal() {
                     className="space-y-4"
                     onSubmit={form.handleSubmit(onHandleSubmit)}
                 >
-                    {/* Email */}
-                    <FormField
-                        control={form.control}
-                        name="email"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Email</FormLabel>
-                                <FormControl>
-                                    <Input placeholder="name@company.com" {...field} className="min-h-[44px]" disabled />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="password"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Password</FormLabel>
-                                <FormControl>
-                                    <Input placeholder="name@company.com" {...field} className="min-h-[44px]" />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
+                    <div className="flex flex-col lg:flex-row items-center gap-2">
+                        <div className="w-full lg:w-1/2">
+                            <FormField
+                                control={form.control}
+                                name="email"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Email</FormLabel>
+                                        <FormControl>
+                                            <Input type="email" placeholder="name@company.com" {...field} className="min-h-[44px]" />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+                        <div className="w-full lg:w-1/2">
+                            <FormField
+                                control={form.control}
+                                name="password"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Password</FormLabel>
+                                        <FormControl>
+                                            <Input type="password" placeholder="name@company.com" {...field} className="min-h-[44px]" />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+                    </div>
 
-                    {/* Name */}
-                    <FormField
-                        control={form.control}
-                        name="name"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Name</FormLabel>
-                                <FormControl>
-                                    <Input placeholder="John Doe" {...field} className="min-h-[44px]" />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
+                    <div className="flex flex-col lg:flex-row items-center gap-4">
+                        <div className="w-full lg:w-1/2">
+                            <FormField
+                                control={form.control}
+                                name="firstName"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>First Name</FormLabel>
+                                        <FormControl>
+                                            <Input type="text" placeholder="John Doe" {...field} className="min-h-[44px]" />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+                        <div className="w-full lg:w-1/2">
+                            <FormField
+                                control={form.control}
+                                name="lastName"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Last Name</FormLabel>
+                                        <FormControl>
+                                            <Input type="text" placeholder="John Doe" {...field} className="min-h-[44px]" />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+                    </div>
 
-                    {/* Image */}
-                    <FormField
-                        control={form.control}
-                        name="image"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Profile Image</FormLabel>
-                                <FormControl>
-                                    <Input
-                                        type="file"
-                                        accept="image/*"
-                                        {...field} className="min-h-[44px]"
-                                    />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-
-                    {/* Position */}
-                    <div className="flex flex-col lg:flex-row items-center gap-2 w-full">
+                    <div className="flex flex-col lg:flex-row items-center gap-4 w-full">
                         <div className="w-full lg:w-1/2">
                             <FormField
                                 control={form.control}
@@ -149,6 +158,7 @@ export default function CreateEditEmpModal() {
                                 )}
                             />
                         </div>
+
                         <div className="w-full lg:w-1/2">
                             <FormField
                                 control={form.control}
@@ -181,8 +191,7 @@ export default function CreateEditEmpModal() {
                         </div>
                     </div>
 
-                    {/* Role */}
-                    <div className="flex flex-col lg:flex-row items-center gap-2">
+                    <div className="flex flex-col lg:flex-row items-center gap-4">
                         <div className="w-full lg:w-1/2">
                             <FormField
                                 control={form.control}
@@ -214,7 +223,6 @@ export default function CreateEditEmpModal() {
                             />
                         </div>
 
-                        {/* Job Type */}
                         <div className="w-full lg:w-1/2">
                             <FormField
                                 control={form.control}
@@ -240,6 +248,72 @@ export default function CreateEditEmpModal() {
                                                 ))}
                                             </SelectContent>
                                         </Select>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col lg:flex-row items-center gap-4">
+                        <div className="w-full lg:w-1/2">
+                            <FormField
+                                control={form.control}
+                                name="accType"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Account Status</FormLabel>
+                                        <Select
+                                            onValueChange={field.onChange}
+                                            defaultValue={field.value}
+                                            value={field.value}
+                                        >
+                                            <FormControl className="w-full">
+                                                <SelectTrigger className="w-full min-h-[44px]">
+                                                    <SelectValue placeholder="Select job type" />
+                                                </SelectTrigger>
+                                            </FormControl>
+                                            <SelectContent>
+                                                {EMP_JOB_TYPES.map((j) => (
+                                                    <SelectItem key={j} value={j}>
+                                                        {j}
+                                                    </SelectItem>
+                                                ))}
+                                            </SelectContent>
+                                        </Select>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+                        <div className="w-full lg:w-1/2">
+                            <FormField
+                                control={form.control}
+                                name="image"
+                                render={() => (
+                                    <FormItem>
+                                        <FormLabel>Avatar Image</FormLabel>
+                                        <FormControl>
+                                            <div className="w-full">
+                                                <label className="inline-block text-white font-medium px-4 py-1.5 rounded-md cursor-pointer transition bg-brand hover:bg-own-hover">
+                                                    Choose File
+                                                    <Input
+                                                        ref={fileInputRef}
+                                                        disabled={isWorking}
+                                                        accept="image/*"
+                                                        type="file"
+                                                        className="hidden"
+                                                        onChange={() => {
+                                                            //* Force re-render to update the file name display
+                                                            form.setValue('image', getFileName())
+                                                        }}
+                                                    />
+                                                </label>
+                                                <span className="ml-3 text-sm">
+                                                    {getFileName()}
+                                                </span>
+                                            </div>
+                                        </FormControl>
                                         <FormMessage />
                                     </FormItem>
                                 )}
