b923d3f9829193002c1af8a3777c36d0a763bb8f
fisheeesh <swanphyo444@gmail.com>
2025-09-03 15:11:38 +0700

    chore: created mood overview quey

--- frontend/src/api/query.ts
+++ frontend/src/api/query.ts
@@ -1,13 +1,26 @@
 import {
     QueryClient,
 } from '@tanstack/react-query'
+import api from '.'
 
 const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
+            // staleTime: 5 * 60 * 1000,
             staleTime: 0
         }
     }
+})
+
+const fetchMoodOverview = async (q?: string | null) => {
+    const query = q ? `?duration=${q}` : '?duration=today'
+    const res = await api.get(`admin/mood-overview${query}`)
+    return res.data
+}
+
+export const moodOverviewQuery = (q?: string | null) => ({
+    queryKey: ['mood-overview', q],
+    queryFn: () => fetchMoodOverview(q)
 })
 
 export default queryClient
