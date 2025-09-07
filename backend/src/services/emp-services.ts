eb0e89868533b5cb2aae28a26295f41cd2a5e6b5
fisheeesh <swanphyo444@gmail.com>
2025-09-08 00:46:19 +0700

    feat: created delete employee by id api route

--- backend/src/services/emp-services.ts
+++ backend/src/services/emp-services.ts
@@ -1,5 +1,34 @@
 import { prisma } from "../config/prisma-client"
 
+export const updateEmpDataById = async (id: number, empData: any) => {
+    const data: any = {
+        firstName: empData.firstName,
+        lastName: empData.lastName,
+        phone: empData.phone,
+        role: empData.role,
+        position: empData.position,
+        jobType: empData.jobType,
+        accType: empData.accType,
+    }
+
+    if (empData.department) {
+        data.department = {
+            connectOrCreate: {
+                where: { name: empData.department },
+                create: { name: empData.department }
+            }
+        }
+    }
+
+    if (empData.avatar) {
+        data.avatar = empData.avatar
+    }
+    return await prisma.employee.update({
+        where: { id },
+        data
+    })
+}
+
 export const getAllEmpEmotionHistory = async (employeeId: number) => {
     return await prisma.emotionCheckIn.findMany({
         where: { employeeId },
@@ -15,5 +44,11 @@ export const getAllEmpEmotionHistory = async (employeeId: number) => {
             }
         },
         orderBy: { createdAt: 'desc' },
+    })
+}
+
+export const deleteEmployeeById = async (id: number) => {
+    return await prisma.employee.delete({
+        where: { id }
     })
 }
