b5d14c80021ba32433847491fc2edc06442d06ec
fisheeesh <swanphyo444@gmail.com>
2025-09-07 17:53:49 +0700

    chore: create reusualbe month year selector comp

--- frontend/src/components/dashboard/month-year-selector.tsx
+++ frontend/src/components/dashboard/month-year-selector.tsx
@@ -1,31 +1,53 @@
-import { MONTHS, YEARS } from "@/lib/constants";
+import { useState } from "react";
+import { useSearchParams } from "react-router";
 
-export default function MonthYearSelector({ mode, setActive, isActive }: {
-    mode: "month" | "year",
-    setActive: (el: string) => void,
-    isActive: (el: string) => boolean
-}) {
-    const array = mode === "month" ? MONTHS : YEARS;
+interface Props {
+    filters: Filter[],
+    filterValue: "ciMonth" | "ciYear"
+}
+
+const now = new Date()
+const currentYear = `${now.getFullYear()}-01-01`;
+const currntMonth = `${now.getFullYear()}-${String(now.getMonth() + 1).padStart(2, "0")}-01`;
+
+export default function MonthYearSelector({ filters, filterValue }: Props) {
+    const [searchParams, setSearchParams] = useSearchParams()
+    const start = filterValue === 'ciYear' ? currentYear : currntMonth
+    const param = searchParams.get(filterValue);
+    const [active, setActive] = useState(param || start);
+
+    const isActive = (value: string) => {
+        return active === value
+    }
+
+    const onHandleClick = (value: string) => {
+        setActive(value)
+        searchParams.set(filterValue, value)
+        if (searchParams.get("ciDate")) searchParams.delete("ciDate")
+        if (filterValue === 'ciMonth' && searchParams.get("ciYear")) searchParams.delete("ciYear")
+        if (filterValue === 'ciYear' && searchParams.get("ciMonth")) searchParams.delete("ciMonth")
+        setSearchParams(searchParams)
+    }
 
     return (
         <div className="flex items-center gap-1.5 flex-wrap">
-            {array.map((el, index) => (
+            {filters.map(({ name, value }) => (
                 <div
-                    onClick={() => setActive(el)}
-                    key={index}
+                    onClick={() => onHandleClick(value)}
+                    key={name}
                     className={`
             p-2 px-3 rounded-md text-sm cursor-pointer capitalize
             transition-colors duration-500 
-            ${mode === "year" && "font-en font-normal"}
-            ${isActive(el)
+            ${filterValue === "ciYear" && "font-en font-normal"}
+            ${isActive(value)
                             ? "bg-blue-500 text-white dark:bg-gray-500 dark:border-gray-500 dark:border"
                             : "bg-gray-100 text-black dark:bg-transparent dark:border  dark:border-slate-300 dark:text-slate-300"
                         } 
-            ${!isActive(el) && "hover:bg-blue-500 hover:text-white dark:hover:bg-gray-400 dark:hover:border-gray-400 dark:hover:text-white"
+            ${!isActive(value) && "hover:bg-blue-500 hover:text-white dark:hover:bg-gray-400 dark:hover:border-gray-400 dark:hover:text-white"
                         }
 `}
                 >
-                    {mode === 'month' ? el.toLowerCase() : el}
+                    {name}
                 </div>
             ))}
         </div>
