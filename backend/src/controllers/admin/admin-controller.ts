58e24494202886ae5fc691617d6251debd7b817b
fisheeesh <swanphyo444@gmail.com>
2025-10-20 00:15:25 +0700

    feat: implemeted mark as completed action plan api route

--- backend/src/controllers/admin/admin-controller.ts
+++ backend/src/controllers/admin/admin-controller.ts
@@ -1,14 +1,14 @@
 import { NextFunction, Request, Response } from "express";
 import { body, validationResult } from "express-validator";
-import { PrismaClient } from "../../../generated/prisma";
+import { PrismaClient, Status } from "../../../generated/prisma";
 import { errorCodes } from "../../config/error-codes";
+import { prisma } from "../../config/prisma-client";
+import { EmailQueue } from "../../jobs/queues/email-queue";
 import { getAdminUserData } from "../../services/admin-services";
 import { getEmployeeById } from "../../services/auth-services";
 import { getAllDepartmentsData } from "../../services/system-service";
 import { checkEmployeeIfNotExits, createHttpErrors } from "../../utils/check";
-import { prisma } from "../../config/prisma-client";
-import { EmailQueue } from "../../jobs/queues/email-queue";
-import { request_body, request_subject } from "../../utils/email-templates";
+import { completion_body, completion_subject, request_body, request_subject } from "../../utils/email-templates";
 
 interface CustomRequest extends Request {
     employeeId?: number
@@ -170,6 +170,100 @@ export const createActionPlan = [
 
         res.status(201).json({
             message: "Successfully created action plan",
+        })
+    }
+]
+
+export const markAsCompletedActionPlan = [
+    body("id", "Action Plan Id is required").isString().trim().notEmpty().escape(),
+    async (req: CustomRequest, res: Response, next: NextFunction) => {
+        const errors = validationResult(req).array({ onlyFirstError: true })
+        if (errors.length > 0) return next(createHttpErrors({
+            message: errors[0].msg,
+            status: 400,
+            code: errorCodes.invalid
+        }))
+
+        const empId = req.employeeId
+        const emp = await getEmployeeById(empId!)
+        checkEmployeeIfNotExits(emp)
+
+        const { id } = req.body
+
+        const actionPlan = await prisma.actionPlan.findUnique({
+            where: { id },
+            select: {
+                criticalId: true,
+                criticalEmployee: {
+                    select: {
+                        employee: {
+                            select: {
+                                id: true,
+                                fullName: true,
+                            }
+                        },
+                    }
+                },
+                type: true,
+            }
+        })
+
+        if (!actionPlan) return next(createHttpErrors({
+            message: "There is no action plan with provided Id.",
+            status: 404,
+            code: errorCodes.notFound
+        }))
+
+        if (actionPlan.type === 'COMPLETED' || actionPlan.type === 'PENDING') return next(createHttpErrors({
+            message: "You cannot mark as completed to Pending/Completed type action plan.",
+            status: 400,
+            code: errorCodes.invalid
+        }))
+
+        await prisma.$transaction(async (tx) => {
+            await tx.actionPlan.update({
+                where: { id },
+                data: {
+                    type: "COMPLETED",
+                    completedAt: new Date()
+                }
+            })
+
+            await tx.criticalEmployee.update({
+                where: { id: actionPlan.criticalId },
+                data: {
+                    resolvedAt: new Date()
+                }
+            })
+
+            await tx.employee.update({
+                where: { id: actionPlan.criticalEmployee.employee.id },
+                data: {
+                    status: Status.WATCHLIST
+                }
+            })
+
+            await tx.notification.create({
+                data: {
+                    avatar: emp!.avatar! ?? "",
+                    toSAdmin: true,
+                    type: "UPDATE",
+                    content: `Action plan for critical employee - ${actionPlan.criticalEmployee.employee.fullName} has been completed by ${emp?.firstName} ${emp?.lastName}. All necessary actions have been taken. ✅`,
+                    departmentId: emp!.departmentId
+                }
+            })
+        })
+
+        await EmailQueue.add('notify-email', {
+            subject: completion_subject(actionPlan.criticalEmployee.employee.fullName),
+            body: completion_body(
+                actionPlan.criticalEmployee.employee.fullName,
+                `${emp?.firstName} ${emp?.lastName}`
+            )
+        })
+
+        res.status(200).json({
+            message: "Successfully updated action plan"
         })
     }
 ]
