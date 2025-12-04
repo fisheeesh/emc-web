6561c7b6eb49963dc6bc7f6a8e277f0c07a5de73
Swam Yi Phyo <swanphyo444@gmail.com>
2025-12-04 16:09:49 +0700

    chore: modified error element display text
    Updated error message to reflect current project status.

--- frontend/src/pages/not-found/error-element.tsx
+++ frontend/src/pages/not-found/error-element.tsx
@@ -6,7 +6,9 @@ export default function ErrorElement() {
         <div className='min-h-screen flex items-center text-sm px-8 md:text-base justify-center text-center flex-col'>
             <img src={error} alt="errr_image" className='size-[350px]' />
             <p className='font-bold mb-2'>Whoops!</p>
-            <p className='max-w-md mx-auto mb-5 text-sm'>The server was terminated because ATA doesn’t provide the server fee for us. Just kidding, my backend isn’t live yet. Come back again at the end of this month and we can give it a shot!</p>
+            <p className='max-w-md mx-auto mb-5 text-sm'>
+                Server's taking a nap since my senior project wrapped up and I'm not paying for hosting anymore. But hey, the app is super cool! Clone it and run it locally to see what it can do. Trust me, it's worth it!
+            </p>
             <Link to='/' className='transition duration-300 rounded-full font-medium hover:text-brand'>
                 &larr; Go Back
             </Link>
